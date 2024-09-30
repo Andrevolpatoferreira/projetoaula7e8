281706e49ecd44a6a75a5372746891bb9e76ed15
Andrevolpatoferreira <161027300+Andrevolpatoferreira@users.noreply.github.com>
2024-09-30 07:56:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'NBA',
+    'O que é NBA?',
+    'National Basketball Association/Associação de basquete nacional, é uma liga de basquete  estadunidense onde os melhores jogadores do mundo jogam'
+)
+
+criaCartao(
+    'Temporada regular',
+    'Quantos jogos cada time joga na temporada regular?',
+    '82 jogos, sendo 4 vezes com times da mesma confêrencia e 2 vexes com times da outra confêrencia.'
+)
+
+criaCartao(
+    'Recorde',
+    'Qual a melhor temporada regular que um time ja fez?',
+    'Golden State Warriors de 2015-2016, eles conseguiram 73 vitórias e apenas 9 derrotas'
+)
+
+criaCartao(
+    'MVP',
+    'Qual foi o MVP de 2023',
+    'O Pivô do Philadelphia 76ers Joel Embiid'
+)
